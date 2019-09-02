9ad7095f9f9134513a0965766dfa3bbf04bb88ed
Your Name <you@example.com>
2019-09-02 11:34:47 +0430

    Render Images on index Page

--- app/models/play.rb
+++ app/models/play.rb
@@ -2,6 +2,6 @@ class Play < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  has_attached_file :play_img, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  has_attached_file :play_img, styles: { :play_index => "250x350>", :play_show => "325x475>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :play_img, content_type: /\Aimage\/.*\z/
 end
