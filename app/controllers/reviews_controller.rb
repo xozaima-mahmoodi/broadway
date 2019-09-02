2ff2d2189a3a7390f2cf3ff5c4d35a9f43022961
Your Name <you@example.com>
2019-09-02 17:59:45 +0430

    Edit and Destroy Action with View File Changes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :find_play
+  before_action :find_review, only: [:edit, :update, :destroy]
   
   
   def new
@@ -20,6 +21,22 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit
+    
+  end
+
+  def update
+    if @review.update(review_params)
+      redirect_to play_path(@play)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to play_path(@play)
+  end
 
   private
 
@@ -29,6 +46,10 @@ class ReviewsController < ApplicationController
 
   def find_play
     @play = Play.find(params[:play_id])
+  end
+
+  def find_review
+    @review = Review.find(params[:id])
   end
 
 
