0069d71f6c7e3115a304dc2e496f303a25c42b5a
Your Name <you@example.com>
2019-09-02 10:15:07 +0430

    Add paperclip to App and Change View Files

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'simple_form'
 gem 'bootstrap-sass'
 gem 'devise'
+gem 'paperclip'
 
 
 # Use Redis adapter to run Action Cable in production
--- app/models/play.rb
+++ app/models/play.rb
@@ -1,4 +1,7 @@
 class Play < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  has_attached_file :play_img, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
